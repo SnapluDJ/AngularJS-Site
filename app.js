8ffe103c9223598312bf7946a94dde35a1a594f9
Dongjie <dongjielu@wustl.edu>
2015-11-16 17:32:54 -0600

    no message

--- app.js
+++ app.js
@@ -1,77 +1,102 @@
-var coffee = angular.module('coffee', ['ngRoute']);
+'use strict';
+
+var coffee = angular.module('coffee', ['ngRoute', 'coffeeController']);
 
 coffee.config(['$routeProvider',
 	function ($routeProvider) {
 		$routeProvider
-			.when	
+			.when('/coffees', {
+				templateUrl: 'coffees.html',
+				controller: 'coffeeCtrl'
+			})	
+			.when('/coffees/:coffeeId', {
+				templateUrl: 'reviews.html',
+				controller: 'coffeeCtrl'
+			})
+			.otherwise({
+				redirectTo: '/coffees'
+			})
 	}]);
 
 
 
+var coffeeController = angular.module('coffeeController', []);
 
-$scope.coffees =
-   [
-   {'id': 1,
-   'brand': "Average Andy's Coffee",
-   'name': 'Regular Coffee',
-   'country': 'Denmark',
-   'reviews': [
-       {'rating': 3,
-       'comment': "Could've been crispier",
-       'reviewer': "Chris P. Bacon"
-       }
-   ]
-   },
-   {'id': 2,
-   'brand': "Jimmy's Coffee",
-   'name': 'Mocha',
-   'country': 'America',
-   'reviews': [
-	   {'rating': 10,
-	   'comment': 'What everyone should drink in the morning!',
-	   'reviewer': 'Earl Lee Riser'
-	   },
-	   {'rating': 10,
-	   'comment': 'A genius of everything coffee',
-	   'reviewer': 'Bob'
-	   }
-   ]
-   },
-   {'id': 3,
-   'brand': "We Did Our Best",
-   'name': 'Latte',
-   'country': 'France',
-   'reviews': [
-	   {'rating': 1,
-	   'comment': " a 'latte' yuckiness.",
-	   'reviewer': 'Tim Burr'
-	   },
-	   {'rating': 1,
-	   'comment': 'Is this even coffee?',
-	   'reviewer': 'Sue Flay'
-	   },
-	    {'rating': 1,
-	   'comment': 'The grossest thing I have ever had.',
-	   'reviewer': 'Myles Long'
-	   },
-	    {'rating': 5,
-	   'comment': 'I dont know what the fuss is about, i dont think its too bad!',
-	   'reviewer': 'Sara Bellum'
-	   }
-   ]
-   },
-   {'id': 4,
-   'brand': "Jimmy's Special Coffee",
-   'name': 'Americano',
-   'country': 'America',
-   'reviews': [
-	   {'rating': 10,
-	   'comment': 'If I could rate it higher, I would!',
-	   'reviewer': 'Justin Case'
-	   },
-	   {'rating': 10,
-	   'comment': 'He does it again!',
-	   'reviewer': 'Eileen Dover'
-	   }
-   ]
-   }];
+coffeeController.controller('coffeeCtrl', ['$scope', '$routeParams', '$rootScope',
+	function ($scope, $routeParams, $rootScope) {
+		$scope.coffees =
+	       [
+	       {'id': 1,
+	       'brand': "Average Andy's Coffee",
+	       'name': 'Regular Coffee',
+	       'country': 'Denmark',
+	       'reviews': [
+	               {'rating': 3,
+	               'comment': "Could've been crispier",
+	               'reviewer': "Chris P. Bacon"
+	               }
+	       ]
+	       },
+	       {'id': 2,
+	       'brand': "Jimmy's Coffee",
+	       'name': 'Mocha',
+	       'country': 'America',
+	       'reviews': [
+		       {'rating': 10,
+		       'comment': 'What everyone should drink in the morning!',
+		       'reviewer': 'Earl Lee Riser'
+		       },
+		       {'rating': 10,
+		       'comment': 'A genius of everything coffee',
+		       'reviewer': 'Bob'
+		       }
+	       ]
+	       },
+	       {'id': 3,
+	       'brand': "We Did Our Best",
+	       'name': 'Latte',
+	       'country': 'France',
+	       'reviews': [
+		       {'rating': 1,
+		       'comment': " a 'latte' yuckiness.",
+		       'reviewer': 'Tim Burr'
+		       },
+		       {'rating': 1,
+		       'comment': 'Is this even coffee?',
+		       'reviewer': 'Sue Flay'
+		       },
+		        {'rating': 1,
+		       'comment': 'The grossest thing I have ever had.',
+		       'reviewer': 'Myles Long'
+		       },
+		        {'rating': 5,
+		       'comment': 'I dont know what the fuss is about, i dont think its too bad!',
+		       'reviewer': 'Sara Bellum'
+		       }
+	       ]
+	       },
+	       {'id': 4,
+	       'brand': "Jimmy's Special Coffee",
+	       'name': 'Americano',
+	       'country': 'America',
+	       'reviews': [
+		       {'rating': 10,
+		       'comment': 'If I could rate it higher, I would!',
+		       'reviewer': 'Justin Case'
+		       },
+		       {'rating': 10,
+		       'comment': 'He does it again!',
+		       'reviewer': 'Eileen Dover'
+		       }
+	       ]
+	       }];
+
+	       for (var c in $scope.coffees) {
+	       		if ($scope.coffees[c].id == $routeParams.coffeeId) {
+	       			$rootScope.selectedCoffee = $scope.coffees[c];
+	       		}
+	       }
+	}]);
+
+
+
